30e0fa760a5ab924f3ed93aae7ebdea699aee2bc
dahlansajith <dahlansajith@gmail.com>
2024-09-20 16:39:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function lettersToNumber(letters) {
             total += letterToNumberMap[letter];
         }
     }
-    return total;
+    return total.join(','); // Join the numbers with a comma
 }
 
 function numbersToLetters(numbers) {
